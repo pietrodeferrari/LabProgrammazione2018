04e2897596e9b1ec90ce8a71b56de39a91e56b12
Marco Casazza <marco.casazza@unimi.it>
2018-11-06 21:29:57 +0100

    Aggiunto esercizio tabelline del Lab03

---
+++ Lab03/tabelline/tabelline.go
@@ -0,0 +1,19 @@
+/*
+Calcola la tabellina di un numero
+
+Letto un numero n da tastiera, il package calcola la sua tabellina
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var numero int
+
+	fmt.Print("Inserisci un numero: ")
+	fmt.Scan(&numero)
+
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i, "x", numero, "=", i*numero)
+	}
+}
